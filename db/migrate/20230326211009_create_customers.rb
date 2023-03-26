6b345e504af243087fd9a2795cf5e8932325b15b
Caleb Thomas <cjthomas00@comcast.net>
2023-03-26 16:40:10 -0600

    update routes for merchant and item API.

--- db/migrate/20230326211009_create_customers.rb
+++ db/migrate/20230326211009_create_customers.rb
@@ -1,6 +1,6 @@
-class CreateCutsomers < ActiveRecord::Migration[7.0]
+class CreateCustomers < ActiveRecord::Migration[7.0]
   def change
-    create_table :cutsomers do |t|
+    create_table :customers do |t|
       t.string :first_name
       t.string :last_name
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_26_170303) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_26_211009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,13 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_170303) do
     t.string "last_name"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
-  end
-
-  create_table "cutsomers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "invoice_items", force: :cascade do |t|

e52d8ddc3380b1f70b8625d808b81037c98f48cb
Caleb Thomas <cjthomas00@comcast.net>
2023-03-27 15:33:53 -0600

    complete tests and code for merchant_items.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -43,8 +43,27 @@ describe "Merchants API" do
     create_list(:item, 25, merchant_id: id)
     get "/api/v1/merchants/#{id}/items"
 
-    merchant = JSON.parse(response.body, symbolize_names: true)
+    items = JSON.parse(response.body, symbolize_names: true)
     
     expect(response).to be_successful
+    
+    expect(items[:data].count).to eq(25)
+
+    items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
+      
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes][:name]).to be_an(String)
+      
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_an(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+    end
   end
 end

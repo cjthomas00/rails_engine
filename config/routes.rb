e945518842972b84cdb586ab546ea01da28de066
Caleb Thomas <cjthomas00@comcast.net>
2023-03-27 19:59:39 -0600

    feat: add #create for items.

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index], controller: "items"
+        resources :items, only: [:index,], controller: "items"
       end
 
-      resources :items, only: [:index, :show]
+      resources :items, only: [:index, :show, :create]
     end
   end
 end

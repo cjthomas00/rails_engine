07bc431f42106d60286628c55ad65fdf9d3eb9ea
Caleb Thomas <cjthomas00@comcast.net>
2023-03-29 11:05:00 -0600

    add more test data to find_all merchants.

--- spec/requests/api/v1/find_merchants_spec.rb
+++ spec/requests/api/v1/find_merchants_spec.rb
@@ -11,7 +11,19 @@ RSpec.describe "Find_merchants" do
 
       expect(response).to be_successful
       parsed_data = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
+      
+      expect(parsed_data[:data]).to be_a(Array)
+      expect(parsed_data[:data].size).to eq(1)
+      parsed_data[:data].each do |data|
+        expect(data).to have_key(:id)
+        expect(data[:id]).to be_an(String)
+        expect(data).to have_key(:type)
+        expect(data[:type]).to be_an(String)
+        expect(data[:type]).to eq("merchant")
+        expect(data[:attributes]).to have_key(:name)
+        expect(data[:attributes][:name]).to be_an(String)
+        expect(data[:attributes][:name]).to eq("Mega-lo Mart")
+      end
     end
   end
 end

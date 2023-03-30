b6984f79225d1e4430a01b9d0e396dd5ec0f3995
Caleb Thomas <cjthomas00@comcast.net>
2023-03-29 18:54:29 -0600

    feat: add route and controller code to search by name.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants/find_all', to: "merchant/search#index"
-      # get '/api/vi/items/find', to: "item/search#show"
+      get '/items/find', to: "item/search#show"
 
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: "items"

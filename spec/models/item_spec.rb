2b00a4fc84a07719f358be0b459f166edc3c72eb
Caleb Thomas <cjthomas00@comcast.net>
2023-03-29 20:55:31 -0600

    test: create min and max price tests.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -23,5 +23,27 @@ RSpec.describe Item, type: :model do
       expect(Item.find_one_by_name("socks")).to eq(item2)
       expect(Item.find_one_by_name("striped")).to eq(item3)
     end
+
+    it "can find one item by minimum price" do
+      item1 = create(:item, name: "Zebra striped socks", unit_price: 99.99)
+      item2 = create(:item, name: "Brown socks", unit_price: 199.99)
+      item3 = create(:item, name: "Mermaid tail socks", unit_price: 299.99)
+
+      expect(Item.search_by_price(199.98, 250.00)).to eq(item2)
+      expect(Item.search_by_price(299.98, 375.00)).to eq(item3)
+      expect(Item.search_by_price(99.98, 150.00)).to eq(item1)
+      expect(Item.search_by_price(399.98, 500)).to eq(nil)
+    end
+
+    it "can find one item by maximum price" do
+      item1 = create(:item, name: "Zebra striped socks", unit_price: 99.99)
+      item2 = create(:item, name: "Brown socks", unit_price: 199.99)
+      item3 = create(:item, name: "Mermaid tail socks", unit_price: 299.99)
+
+      expect(Item.search_by_price(25.00, 199.98)).to eq(item1)
+      expect(Item.search_by_price(150.00, 299.98)).to eq(item2)
+      expect(Item.search_by_price(25.00, 99.98)).to eq(nil)
+      expect(Item.search_by_price(250.00, 399.98)).to eq(item3)
+    end
   end
 end
